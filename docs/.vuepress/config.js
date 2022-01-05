3fd49b25a0a17477ae7ec0bd5f9e9ef9885c63d3
Sankar boro <sankar.boro@zeeve.io>
2022-01-05 11:45:43 +0530

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: 'Frontier',
+  description: 'Ethereum compatibility layer for Substrate',
+  base: '/frontier/',
+
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  themeConfig: {
+    repo: 'https://github.com/axia-dev/frontier',
+    editLinks: false,
+    docsDir: 'docs',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      { text: 'API reference', link: 'https://axia-dev.github.io/frontier/rustdocs/pallet_evm' }
+    ],
+    sidebar: [
+      'overview',
+      'frame/evm',
+      'frame/ethereum'
+    ]
+  },
+
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
